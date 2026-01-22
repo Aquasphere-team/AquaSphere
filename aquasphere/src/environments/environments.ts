6092e81fd5ed1869500669403c85e0a923778a54
danhohe <dani.hoheneder@gmail.com>
2026-01-22 20:36:25 +0100

    Fix: increase build budgets or other fix

--- aquasphere/src/environments/environments.ts
+++
@@ -1,7 +0,0 @@
-// environment file für Supabase cloud backend
-
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://mofgsjoeaezitumlcnio.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vZmdzam9lYWV6aXR1bWxjbmlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MjQ2MjcsImV4cCI6MjA3OTQwMDYyN30.Gptgx0O6ah0fO41bJmP9PYcJMYa9b510oepONz5IU7I'
-};
